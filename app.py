234374f8dc5d47dd112a0d09c6f9bcc39a9e01b2
helloKittyCatSweet <109123755+helloKittyCatSweet@users.noreply.github.com>
2025-12-06 07:19:49 +0000

    添加用户日志的CRUD，用到SQLite3数据库

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import sqlite3
+import datetime
 
 app = Flask(__name__)
 
@@ -10,11 +12,48 @@ def index():
 def main():
     q = request.form.get("q")
     print(q)
+
+    name = q
+    timestamp = datetime.datetime.now()
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute("insert into user (name, timestamp) values (?,?)", (name, timestamp))
+    conn.commit()
+    c.close()
+    conn.close()
+
     return (render_template("main.html"))
 
 @app.route("/paynow", methods=["GET", "POST"])
 def paynow():
     return (render_template("paynow.html"))
+
+@app.route("/userlog", methods=["GET", "POST"])
+def userlog():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute("select * from user")
+    r = ""
+    for row in c:
+        r = r + str(row)
+    print(r)
+
+    c.close()
+    conn.close()
+
+    return (render_template("userlog.html", r = r))
+
+@app.route("/delete-userlog", methods=["GET", "POST"])
+def delteUserLog():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute("delete from user")
+    conn.commit()
+
+    c.close()
+    conn.close()
+
+    return (render_template("delete-userlog.html"))
 
 if __name__ == "__main__":
     app.run()
